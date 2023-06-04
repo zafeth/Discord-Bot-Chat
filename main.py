1e054c1e077e12dcd475aa7b0afc4e49389e6c29
Zaff.dev <32251615+zafeth@users.noreply.github.com>
2023-06-04 16:08:44 +0700

    Update main.py
    Add more tools auto delete message after send message, and not detect used bot

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 import os
+import asyncio
+import discord
 
 os.system('pip install -U discord==1.7.3')
 os.system('pip install -U discord.py==1.7.3')
 
-import discord, asyncio
-
-token = "" #tokenid
-replyMessage = 'Hello seilors ahoy'
-channelId = 00000000
-delay = 61
+token = "xxxx" #TokenIdYour
+replyMessage = 'Hallo bang'
+channelId = 111111 #channeild
+delay = 15
 
 mainMessages = [
-'Hello seilors',
-'Seilors ahhoy',
-'Ah dont spam keep calm sir',
-'Dont forget keep calm',
-'Experience the power of community with SeiNetwork!',
-'SeiNetworkunlocking your full potential!',
-'At SeiNetwork, we never stop striving for excellence',
-'Join SeiNetwork and discover a world of possibilities',
-'Sei can killer ethereum',
-'At SeiNetwork, we believe in pushing boundaries and breaking limits',
-'Join us and be a part of the SeiNetwork family',
-'Hello everyone seilors ahoy',
-'SeiNetwork where innovation and creativity thrive!',
-'Unlock your potential with the support of SeiNetwork',
-'At SeiNetwork, we believe in the power of collaboration and teamwork',
-'Join SeiNetwork and let us help you achieve your goals',
-'SeiNetwork where we celebrate diversity and inclusivity',
-'Good project SeiNetwork',
-'Keep active sir and get role',
-'dont spaming oke ?'
+    'hadeh',
+    'gw baru join ni bang',
+    'gimana cara naik levelnya',
+    'infonya dong bang cara dapet airdrop',
+    'ajarin gw bang cara garapnya',
+    'sombonk amat ini',
+    'oke gw udh mulai paham',
+    'gw paham cara ngerjainnya',
+    'aduh cape',
+    'gw mau menyerah aja deh'
 ]
-
 
 class Main(discord.Client):
     async def on_ready(self):
@@ -40,20 +29,22 @@ class Main(discord.Client):
         while True:
             channel = self.get_channel(channelId)
             for i, msg in enumerate(mainMessages):
-                await channel.send(msg)
+                sent_message = await channel.send(msg)
                 print(f'Sent message {i+1} in #{channel.name}.')
                 await asyncio.sleep(delay)
-
+                await sent_message.delete()  # Delete the sent message
 
     async def on_message(self, message):
         if isinstance(message.channel, discord.DMChannel):
             if message.author.id != self.user.id:
-                with open('blacklist.txt', 'r', encoding = 'UTF-8') as file:
+                with open('blacklist.txt', 'r', encoding='UTF-8') as file:
                     if str(message.author.id) not in file.read():
-                        await message.reply(replyMessage)
+                        sent_message = await message.reply(replyMessage)
                         print('Replied to %s.' % message.author.name)
-                        with open('blacklist.txt', 'a', encoding = 'UTF-8') as file:
+                        await asyncio.sleep(delay)
+                        await sent_message.delete()  # Delete the sent message
+                        with open('blacklist.txt', 'a', encoding='UTF-8') as file:
                             file.write('%s\n' % message.author.id)
 
 if __name__ == '__main__':
-    Main().run(token, bot = False)
+    Main().run(token, bot=False)
